efe63f32b6c0778b88cf372b8f0a255b2aa03b9c
Wen <l1048445160@live.com>
2020-07-18 21:01:45 +0800

    updated Python400/p08/chapter02/lesson035-039, branch and loop statement.

--- Python400/p08/chapter02_jinja2/lesson035-039_branch_and_loop/app.py
+++ Python400/p08/chapter02_jinja2/lesson035-039_branch_and_loop/app.py
@@ -8,7 +8,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', age=19)
+    kwargs = {
+        'age': 19,
+        'users': ['KANA', 'AKI', 'INORI'],
+        'person': {
+            'Name': 'KANA',
+            'Age': 30,
+            'Sex': 'Female'
+        }
+    }
+    return render_template('index.html', **kwargs)
 
 
 if __name__ == '__main__':
