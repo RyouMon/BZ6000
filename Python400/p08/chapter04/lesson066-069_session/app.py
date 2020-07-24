a1d11dbad3fa42f8d6606dc6c73118f52e995bdd
Wen <l1048445160@live.com>
2020-07-24 12:43:23 +0800

    updated Python400/p08/chapter04/lesson066-069, session.

---
+++ Python400/p08/chapter04/lesson066-069_session/app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from flask import Flask, session, request
+from flask import url_for, escape, redirect, render_template
+
+app = Flask(__name__)
+
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return 'Logged in as {}'.format(escape(session['username']))
+    return 'You are not logged in'
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
