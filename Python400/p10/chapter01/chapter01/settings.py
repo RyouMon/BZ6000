26bb4a8798f7802d8c0150fea5f13983bc597c14
Wen <l1048445160@live.com>
2020-07-31 09:00:48 +0800

    updated Python400/p10/chapter01, many-to-one relationships.

--- Python400/p10/chapter01/chapter01/settings.py
+++ Python400/p10/chapter01/chapter01/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'process_http_request.apps.ProcessHttpRequestConfig',
+    'model.apps.ModelConfig'
 ]
 
 MIDDLEWARE = [
--- Python400/p10/chapter01/model/models.py
+++ Python400/p10/chapter01/model/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+# many-to-one relationships
+class Manufacture(models.Model):
+    name = models.CharField(max_length=10)
+
+
+class Car(models.Model):
+    product = models.CharField(max_length=10)
+    manufacture = models.ForeignKey('Manufacture', on_delete=models.CASCADE)
