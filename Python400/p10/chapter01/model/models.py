6e3e5073332e21a84a570fff2c8b7da9a114c6d4
Wen <l1048445160@live.com>
2020-07-31 12:34:09 +0800

    updated Python400/p10/chapter01, custom intermediary table.

--- Python400/p10/chapter01/model/models.py
+++ Python400/p10/chapter01/model/models.py
@@ -21,3 +21,25 @@ class Topping(models.Model):
 class Pizza(models.Model):
     name = models.CharField(max_length=10)
     toppings = models.ManyToManyField(Topping)
+
+
+# many-to-many relationships
+# through a custom intermediary table
+class Person(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=128)
+    members = models.ManyToManyField(
+        Person,
+        through='Membership',
+        through_fields=('group', 'person'),
+    )
+
+
+class Membership(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    level = models.IntegerField(default=1)
