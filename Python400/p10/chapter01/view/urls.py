45d6f2670966832523dcd5b9ee0b993d0521d9f7
Wen <l1048445160@live.com>
2020-08-02 22:25:06 +0800

    updated Python400/p10/chapter01, my first class-based view.

---
+++ Python400/p10/chapter01/view/urls.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('get/', views.MyView.as_view()),
+]
--- Python400/p10/chapter01/view/views.py
+++ Python400/p10/chapter01/view/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.views import View
+from django.http import HttpResponse
 # Create your views here.
+
+
+class MyView(View):
+    def get(self, request):
+        return HttpResponse('Response by class-based view.')
