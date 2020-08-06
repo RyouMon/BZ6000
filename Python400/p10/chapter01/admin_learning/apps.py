48c6ea9fef6d7474b7927be5c7439915d8b7c8ff
wen-surface <l1048445160@live.com>
2020-08-06 23:11:24 +0800

    updated Python400/p10/chapter01, learn admin site, part 1.

--- Python400/p10/chapter01/admin_learning/apps.py
+++ Python400/p10/chapter01/admin_learning/apps.py
@@ -3,3 +3,5 @@ from django.apps import AppConfig
 
 class AdminLearningConfig(AppConfig):
     name = 'admin_learning'
+    verbose_name = '学习管理后台'
+
--- Python400/p10/chapter01/admin_learning/models.py
+++ Python400/p10/chapter01/admin_learning/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Place(models.Model):
+    city = models.CharField(max_length=64, verbose_name='城市')
+    address = models.CharField(max_length=128, verbose_name='详细地址')
+
+    class Meta:
+        verbose_name_plural = verbose_name = '地址'
+
+    def __str__(self):
+        return f'{self.city} {self.address}'
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=128, verbose_name='店名')
+    place = models.OneToOneField(
+        Place,
+        on_delete=models.CASCADE,
+        primary_key=True,
+        verbose_name='地址',
+    )
+    serves_hot_dogs = models.BooleanField(default=False, verbose_name='售卖热狗')
+    serves_pizza = models.BooleanField(default=False, verbose_name='售卖Pizza')
+
+    class Meta:
+        verbose_name_plural = verbose_name = '饭店'
+
+    def __str__(self):
+        return f'饭店（{self.name}）'
+
+
+class Waiter(models.Model):
+    name = models.CharField(max_length=128, verbose_name='姓名')
+    restaurant = models.ForeignKey(
+        Restaurant,
+        on_delete=models.CASCADE,
+        verbose_name='所属饭店'
+    )
+
+    class Meta:
+        verbose_name_plural = verbose_name = '服务员'
+
+    def __str__(self):
+        return f'服务员（{self.name}）'
