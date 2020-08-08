ea1f5db2d3894cee141116982ba7a492663a13a8
Wen <l1048445160@live.com>
2020-08-08 08:41:01 +0800

    updated Python400/p10/chapter01, use custom model admin.

--- Python400/p10/chapter01/admin_learning/admin.py
+++ Python400/p10/chapter01/admin_learning/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import *
 
+
+@admin.register(Waiter)
+class WaiterAdmin(admin.ModelAdmin):
+    list_display = ['name', 'age', 'sex']
+    ordering = ['age']
+
+
 # Register your models here.
 admin.site.register(Place)
 admin.site.register(Restaurant)
-admin.site.register(Waiter)
+# admin.site.register(Waiter, WaiterAdmin)
