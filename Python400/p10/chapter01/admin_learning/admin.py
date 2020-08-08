450c4cf41b47a205b304185854afbddeec875ff6
Wen <l1048445160@live.com>
2020-08-08 09:24:09 +0800

    updated Python400/p10/chapter01, writing actions.

--- Python400/p10/chapter01/admin_learning/admin.py
+++ Python400/p10/chapter01/admin_learning/admin.py
@@ -2,10 +2,16 @@ from django.contrib import admin
 from .models import *
 
 
+def age_plus_one(modeladmin, request, queryset):
+    queryset.update(age=models.F('age')+1)
+    age_plus_one.short_description = '年龄加1'
+
+
 @admin.register(Waiter)
 class WaiterAdmin(admin.ModelAdmin):
     list_display = ['name', 'age', 'sex']
     ordering = ['age']
+    actions = [age_plus_one]
 
 
 # Register your models here.
