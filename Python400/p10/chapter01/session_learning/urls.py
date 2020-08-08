42c3bb49ab698380dda83cb902b9c6173184cf0e
Wen <l1048445160@live.com>
2020-08-08 20:40:06 +0800

    updated Python400/p10/chapter01, adding url rules.

--- Python400/p10/chapter01/session_learning/urls.py
+++ Python400/p10/chapter01/session_learning/urls.py
@@ -4,6 +4,12 @@ from django.urls import path
 from . import views
 
 
+app_name = 'session_learning'
+
+
 urlpatterns = [
     path('test/', views.test_session),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('index/', views.index, name='index'),
 ]
