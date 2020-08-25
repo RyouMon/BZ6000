55331488c8bb08d08c75dbfededbb02e884231b1
Wen <l1048445160@live.com>
2020-08-25 20:24:57 +0800

    Update Python400/p10/chapter03. Create model Student, add new serializer StudentSerializer.

--- Python400/p10/chapter03/rest_framework_learning/models.py
+++ Python400/p10/chapter03/rest_framework_learning/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    SEX_CHOICES = ((0, 'Male'), (1, 'Female'))
+
+    name = models.CharField(max_length=20, verbose_name='Name')
+    age = models.IntegerField(null=True, blank=True, verbose_name='Age')
+    sex = models.IntegerField(choices=SEX_CHOICES, default=0, verbose_name='Sex')
