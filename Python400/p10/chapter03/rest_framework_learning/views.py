309fb105b4afa5edc7ff63107a388ef561756012
wen-surface <l1048445160@live.com>
2020-08-27 21:17:27 +0800

    Update Python400/p10/chapter03. Use Request, Response and api_view decorator, add optional format suffixes to urls.

--- Python400/p10/chapter03/rest_framework_learning/views.py
+++ Python400/p10/chapter03/rest_framework_learning/views.py
@@ -1,97 +1,93 @@
-from django.views.decorators.csrf import csrf_exempt
-from django.http import JsonResponse, HttpResponse
-from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 from . import models
 from . import serializers
 
 
-@csrf_exempt
-def students(request):
+@api_view(['GET', 'POST'])
+def students(request, format=None):
     """
     list all students, or create a new student.
     """
     if request.method == 'GET':
         student_li = models.Student.objects.all()
         serializer = serializers.StudentSerializer(student_li, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = serializers.StudentSerializer(data=data)
+        serializer = serializers.StudentSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(request.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
-def student_detail(request, pk):
+@api_view(['GET', 'PUT', 'DELETE'])
+def student_detail(request, pk, format=None):
     """
     Retrieve, update or delete a student.
     """
     try:
         student = models.Student.objects.get(pk=pk)
     except models.Student.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = serializers.StudentSerializer(student)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = serializers.StudentSerializer(student, data=data)
+        serializer = serializers.StudentSerializer(student, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, safe=True)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         student.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@csrf_exempt
-def groups(request):
+@api_view(['GET', 'POST'])
+def groups(request, format=None):
     """
     list all groups, or create a new group.
     """
     if request.method == 'GET':
         group_li = models.Group.objects.all()
         serializer = serializers.GroupSerializer(group_li, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = serializers.GroupSerializer(data=data)
+        serializer = serializers.GroupSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(request.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
-def group_detail(request, pk):
+@api_view(['GET', 'PUT', 'DELETE'])
+def group_detail(request, pk, format=None):
     """
     Retrieve, update or delete a group.
     """
     try:
         group = models.Group.objects.get(pk=pk)
     except models.Group.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = serializers.GroupSerializer(group)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = serializers.GroupSerializer(group, data=data)
+        serializer = serializers.GroupSerializer(group, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, safe=True)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         group.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
